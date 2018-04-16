4d3acde0785243b1ba84d0ff301f24a8ad996fb7
Angus Malloch <angusbass@hotmail.com>
2018-04-16 19:34:00 +0100

    Can't see where i'm going wrong atm. Don't think any routes are working because when I type /films it brings back the data.

--- controllers/films.js
+++ controllers/films.js
@@ -5,12 +5,31 @@ var Review = require('../client/src/models/review');
 var express = require('express');
 var filmRouter = new express.Router();
 
+filmRouter.get('/', function(req, res) {
+  res.json({data: films});
+});
+
+//show
 filmRouter.get('/films/:id', function(req, res){
   res.json({data: films[req.params.id]});
 })
 
-filmRouter.get('/', function(req, res) {
-  res.json(films);
-});
+//create
+filmRouter.post('/films', function(req, res) {
+  films.push(req.body.film);
+  res.json({data: films});
+})
+
+//update
+filmRouter.put('/films/:id', function(req, res) {
+  films[req.params.id] = req.body.film;
+  res.json({data: films});
+})
+
+// delete
+filmRouter.delete('/films/:id', function(req, res) {
+  films.splice(req.params.id, 1);
+  res.json({data: films});
+})
 
 module.exports = filmRouter;
--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,16 @@
 var express = require('express');
 var router = new express.Router();
 
-router.use('/films', require('./films'));
+router.use('/films', require('./films.js'));
+// router.use('/film', require('./film.js'));
+// router.use('/review', require('./review.js'));
 
 router.get('/', function (req, res) {
   res.json({data: "Welcome"});
+})
+
+router.get('/about', function(req, res) {
+  res.json({data: "All about us!"})
 })
 
 module.exports = router;
